4cf72a86edfaec7c87724fb4561031c93ee2c407
Ange Near <handofbug@gmail.com>
2017-11-03 14:40:05 +0300

    sdf

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ var app = express();
 app.set('port', (process.env.PORT || 5000));
 
 app.get('/', function(request, response) {
-  response.end('hell');
+  response.end('hello');
 });
 
 app.listen(app.get('port2'), function() {
