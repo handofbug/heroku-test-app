a53fddb8f4af7757dde140097f5418912605b8e4
Ange Near <handofbug@gmail.com>
2017-11-03 16:27:50 +0300

    pass

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 var express = require('express');
-var db = require('monk')('test:test@ds245715.mlab.com:45715/heroku');
+var db = require('monk')(process.env.DB_USER + ':' + process.env.DB_PASS +'@ds245715.mlab.com:45715/heroku');
 var app = express();
 var users = db.get('test');
 app.set('port', (process.env.PORT || 5000));
