67f7f00dd07f394da2fbc1780f535ecf4e45b598
Ange Near <handofbug@gmail.com>
2017-11-03 15:05:15 +0300

    db

--- index.js
+++ index.js
@@ -1,12 +1,19 @@
 var express = require('express');
+var db = require('monk')('test:test@ds245715.mlab.com:45715/heroku');
 var app = express();
-
+var users = db.get('test');
 app.set('port', (process.env.PORT || 5000));
 
-app.get('/', function(request, response) {
-  response.end('hello');
+app.get('/', function (request, response) {
+    users.find({}).then((data) => {
+        console.log(data);
+        response.end(JSON.stringify(data));
+    });
 });
-
-app.listen(app.get('port'), function() {
-  console.log('Node app is running on port', app.get('port'));
+app.get('/insert', function (request, response) {
+    users.insert({'name':'name'});
+        response.end('');
+});
+app.listen(app.get('port'), function () {
+    console.log('Node app is running on port', app.get('port'));
 });
